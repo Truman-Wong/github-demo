336e0f3d28934e3addc6aaa1c6f5ae4e3264898a
truman-wang <truman.wang@tigergraph.com>
2022-09-24 01:33:34 +0000

    fix interpret_results.py

--- interpret_results.py
+++ interpret_results.py
@@ -1,3 +1,4 @@
+#to use sum, just replace mean with sum
 import pandas as pd
 df = pd.read_csv("timings.csv", sep='|')
 df2 = df[df.day=='2012-11-29'].groupby('q', sort=False).mean()
@@ -10,4 +11,5 @@ print(df3)
 print("")
 df4 = df[df.day=='2012-12-01'].groupby('q', sort=False).mean()
 print("batch 2012-12-01")
+print(df4)
 print("")
