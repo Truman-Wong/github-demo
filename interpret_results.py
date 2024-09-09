40d1456afb4d657734850fda01b9b64f74b6c6fe
trumanWangtg <108296682+trumanWangtg@users.noreply.github.com>
2024-09-09 11:44:38 -0700

    Update interpret_results.py

--- interpret_results.py
+++ interpret_results.py
@@ -1,15 +1,21 @@
 #to use sum, just replace mean with sum
 import pandas as pd
+
+# Read the CSV file
 df = pd.read_csv("timings.csv", sep='|')
-df2 = df[df.day=='2012-11-29'].groupby('q', sort=False).mean()
+
+# Filter and group by day and q, then calculate the mean for the 'time' column
+df2 = df[df['day'] == '2012-11-29'].groupby('q', sort=False)['time'].mean()
 print("batch 2012-11-29")
 print(df2)
 print("")
-df3 = df[df.day=='2012-11-30'].groupby('q', sort=False).mean()
+
+df3 = df[df['day'] == '2012-11-30'].groupby('q', sort=False)['time'].mean()
 print("batch 2012-11-30")
 print(df3)
 print("")
-df4 = df[df.day=='2012-12-01'].groupby('q', sort=False).mean()
+
+df4 = df[df['day'] == '2012-12-01'].groupby('q', sort=False)['time'].mean()
 print("batch 2012-12-01")
 print(df4)
 print("")
